0984b3ef8c11f0c877b140ac7ca09309691de94e
Mark T. Holder <mtholder@gmail.com>
2014-11-13 10:29:55 +0100

    setup.py based steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,12 @@ then
     virtualenv env || exit
 fi
 . env/bin/activate || exit
-pip install -r requirements.txt
+pip install -r requirements.txt || exit
+cd jds 
+python setup.py develop || exit
 
 echo "You will need to:"
 echo "  . env/bin/activate"
+echo "and then run:"
+echo "  pserve development.ini --reload"
+echo "from $PWD to run the server in debug mode (not safe for deployment)."
